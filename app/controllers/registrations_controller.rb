16bbe9c45d979cef008109c5eb47686c3361c202
cwilliams3579 <clevelandwilliams3579@gmail.com>
2016-12-25 14:09:31 -0600

    made changes to registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+
   def create
     build_resource(sign_up_params)
-
     resource.class.transaction do
       resource.save
       yield resource if block_given?
+
       if resource.persisted?
-        @payment = Payment.new({email: params["user"]["email"],
+        @payment = Payment.new({ email: params["user"]["email"],
           token: params[:payment]["token"], user_id: resource.id })
           flash[:error] = "Please check registration errors" unless @payment.valid?
-
           begin
             @payment.process_payment
             @payment.save
           rescue Exception => e
             flash[:error] = e.message
-
             resource.destroy
             puts 'Payment failed'
             render :new and return
@@ -43,4 +42,5 @@ class RegistrationsController < Devise::RegistrationsController
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up).push(:payment)
     end
+
   end
